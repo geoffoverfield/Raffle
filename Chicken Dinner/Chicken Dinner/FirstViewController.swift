f4f787bc9b55ca250bc4c1867f47f140c669af66
Geoff Overfield <geoff.overfield@gmail.com>
2020-01-12 20:04:24 -0700

    Initial commit
    Application shell with no functionality

---
+++ Chicken Dinner/Chicken Dinner/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Chicken Dinner
+//
+//  Created by Geoffrey Overfield on 1/12/20.
+//  Copyright © 2020 Geoffrey Overfield. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Chicken Dinner/Chicken Dinner/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Chicken Dinner
+//
+//  Created by Geoffrey Overfield on 1/12/20.
+//  Copyright © 2020 Geoffrey Overfield. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
